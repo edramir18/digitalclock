455ce15f9c33826eb334271924d89b692d858e70
Eduardo Ramirez <edramir18@gmail.com>
2018-01-18 16:10:38 -0400

    Working on event listener

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-function endUpTransition(e){
+/*function endUpTransition(e){
     const f = e.target;        
     const b = f.parentNode.querySelector(".b-clock__flip--back");
     f.classList.remove("b-clock__flip--front");
@@ -27,14 +27,42 @@ function tickClock(up, down){
     
     upBack.textContent = second;
     downFront.textContent = second;    
+}*/
+
+function tickClock(letter){
+    const now = new Date();
+    const top = letter.children[0];
+    const bottom = letter.children[1];    
+    
+    const topFront = top.querySelector(':not(.-isback)');
+    const topBack = top.querySelector('.-isback');
+
+    topFront.classList.add('-isrotated');
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
     
-    const upLetter = document.querySelector("#flex-top>.b-clock__letter");
+    const letter = document.querySelector(".letter");
+    const layers = letter.querySelectorAll('.l-layer');
+    for(let i=0; i < layers.length; i++){
+        console.log(layers[i]);
+        layers[i].addEventListener("transitionend",(e) => {
+            if (e.propertyName !== 'transform') return;
+            const layer = e.target;
+            if (layer.classList.contains('.-isdown')){
+
+            }else {
+                layer.classList.add('.-isback');
+            }
+        });
+    }
+
+    setInterval(tickClock,1000, letter);
+
+    /*const upLetter = document.querySelector("#flex-top>.b-clock__letter");
     const downLetter = document.querySelector("#flex-bottom>.b-clock__letter");
     
     setTimeout(tickClock, 2500, upLetter, downLetter);
-    //setInterval(tickClock, 1000, upLetter, downLetter);
+    //setInterval(tickClock, 1000, upLetter, downLetter);*/
 
 });
