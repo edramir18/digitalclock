0f008cd9594dca229cc7850088c4b72f7e3a4df0
Eduardo Ramirez <edramir18@gmail.com>
2018-01-18 13:38:04 -0400

    Draft incomplete

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,40 @@
+function endUpTransition(e){
+    const f = e.target;        
+    const b = f.parentNode.querySelector(".b-clock__flip--back");
+    f.classList.remove("b-clock__flip--front");
+    b.classList.remove("b-clock__flip--back");
+    b.classList.add("b-clock__flip--front");
+    f.classList.add("b-clock__flip--back");
+    f.classList.remove("b-clock__flip--rotate");
+    f.removeEventListener("transitioned", endUpTransition);
+    const event = new CustomEvent("finished");
+}
+
+function startDownTransition(e){
+    console.log("It's Working!!!");
+    console.log(e.target);
+}
+
+function tickClock(up, down){
+    const now = new Date();
+    const second = now.getSeconds() % 10;
+    const upFront = up.querySelector(".b-clock__flip--front");    
+    const upBack = up.querySelector(".b-clock__flip--back");
+    const downFront = down.querySelector(".b-clock__flip--front");
+    const downBack = down.querySelector(".b-clock__flip--back");
+    
+    if(!upFront || !upBack || !downFront || !downBack) return;
+    
+    upBack.textContent = second;
+    downFront.textContent = second;    
+}
+
 document.addEventListener("DOMContentLoaded", (e) => {
-    console.log("DOM Loaded");
-    const upLetter = document.querySelector(".b-clock__flip--up");
-    console.log(upLetter);
-    upLetter.classList.add("b-clock__flip--rotate");
+    
+    const upLetter = document.querySelector("#flex-top>.b-clock__letter");
+    const downLetter = document.querySelector("#flex-bottom>.b-clock__letter");
+    
+    setTimeout(tickClock, 2500, upLetter, downLetter);
+    //setInterval(tickClock, 1000, upLetter, downLetter);
+
 });
